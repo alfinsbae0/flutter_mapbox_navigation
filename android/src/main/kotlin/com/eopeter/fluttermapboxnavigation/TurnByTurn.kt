47c20066f67ddf367bdd2895077ffc5b5c9ed243
alfinsbae0 <108179538+alfinsbae0@users.noreply.github.com>
2024-11-19 11:10:30 +0700

    update clearRoute Android

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/TurnByTurn.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/TurnByTurn.kt
@@ -177,6 +177,7 @@ open class TurnByTurn(
         val navigation = MapboxNavigationApp.current()
         navigation?.stopTripSession()
         PluginUtilities.sendEvent(MapBoxEvents.NAVIGATION_CANCELLED)
+        result.success(true)
     }
 
     private fun startFreeDrive() {
